b57290e4f2caa381ae95d1f261fe937f5a04b2ef
Tejas Nikhar <tejastn10@gmail.com>
2024-11-15 19:50:28 +0530

    fix: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: ["class"],
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 	prefix: "",
 	theme: {
 		container: {
