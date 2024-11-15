bd98fbba302ba91fa89a25f8ab99f8e0d7d24813
Tejas Nikhar <tejastn10@gmail.com>
2024-11-15 18:46:45 +0530

    feat: add Navbar container

---
+++ src/containers/Navbar.tsx
@@ -0,0 +1,81 @@
+import { FC } from "react";
+
+import Link from "next/link";
+
+import { ThemeToggle } from "../components/theme/ThemeToggle";
+
+import { Dock, DockIcon } from "../components/animated/Dock";
+
+import { Separator } from "../components/ui/Separator";
+import { buttonVariants } from "../components/ui/Button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../components/ui/Tooltip";
+
+import { DATA } from "@/data/resume";
+
+import { combineClasses } from "@/utils/tailwind";
+
+const Navbar: FC = () => {
+	return (
+		<div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
+			<div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
+			<Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
+				{DATA.navbar.map((item, index) => (
+					<DockIcon key={index}>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href={item.href}
+									className={combineClasses(
+										buttonVariants({ variant: "ghost", size: "icon" }),
+										"size-12"
+									)}
+								>
+									<item.icon className="size-4" />
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p>{item.label}</p>
+							</TooltipContent>
+						</Tooltip>
+					</DockIcon>
+				))}
+				<Separator orientation="vertical" className="h-full" />
+				{Object.entries(DATA.contact.social)
+					.filter(([, social]) => social.navbar)
+					.map(([name, social]) => (
+						<DockIcon key={name}>
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<Link
+										href={social.url}
+										className={combineClasses(
+											buttonVariants({ variant: "ghost", size: "icon" }),
+											"size-12"
+										)}
+									>
+										<social.icon className="size-4" />
+									</Link>
+								</TooltipTrigger>
+								<TooltipContent>
+									<p>{name}</p>
+								</TooltipContent>
+							</Tooltip>
+						</DockIcon>
+					))}
+				<Separator orientation="vertical" className="h-full py-2" />
+				<DockIcon>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<ThemeToggle />
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>Theme</p>
+						</TooltipContent>
+					</Tooltip>
+				</DockIcon>
+			</Dock>
+		</div>
+	);
+};
+
+export { Navbar };
