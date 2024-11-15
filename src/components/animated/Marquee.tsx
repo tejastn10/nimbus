3b627bb9cefdb31e36d6b446dabaaef2faa4f3e8
Tejas Nikhar <tejastn10@gmail.com>
2024-11-15 17:46:36 +0530

    feat: add animated Marquee component

---
+++ src/components/animated/Marquee.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+
+import { combineClasses } from "@/utils/tailwind";
+
+type MarqueeProps = {
+	repeat?: number;
+	reverse?: boolean;
+	className?: string;
+	vertical?: boolean;
+	pauseOnHover?: boolean;
+	children?: React.ReactNode;
+};
+
+const Marquee: FC<MarqueeProps> = ({
+	reverse,
+	children,
+	className,
+	repeat = 4,
+	vertical = false,
+	pauseOnHover = false,
+	...props
+}) => {
+	return (
+		<div
+			{...props}
+			className={combineClasses(
+				"group flex overflow-hidden p-1 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+				{
+					"flex-row": !vertical,
+					"flex-col": vertical,
+				},
+				className
+			)}
+		>
+			{Array(repeat)
+				.fill(0)
+				.map((_, i) => (
+					<div
+						key={i}
+						className={combineClasses("flex shrink-0 justify-around [gap:var(--gap)]", {
+							"animate-marquee flex-row": !vertical,
+							"animate-marquee-vertical flex-col": vertical,
+							"group-hover:[animation-play-state:paused]": pauseOnHover,
+							"[animation-direction:reverse]": reverse,
+						})}
+					>
+						{children}
+					</div>
+				))}
+		</div>
+	);
+};
+
+export { Marquee };
