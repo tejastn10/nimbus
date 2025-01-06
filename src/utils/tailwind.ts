0eb6af6ddb77142147fc421c58c1b90942cff451
Tejas Nikhar <tejastn10@gmail.com>
2025-01-06 08:15:16 +0530

    refactor: rename combineClasses to cx for improved clarity

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -2,8 +2,8 @@ import { type ClassValue, clsx } from "clsx";
 
 import { twMerge } from "tailwind-merge";
 
-const combineClasses = (...inputs: ClassValue[]): string => {
+const cx = (...inputs: ClassValue[]): string => {
 	return twMerge(clsx(inputs));
 };
 
-export { combineClasses };
+export { cx };
