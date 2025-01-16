ec27d122344512b06d649cef57760d0a912d21ed
Tejas Nikhar <tejastn10@gmail.com>
2025-01-16 06:06:07 +0530

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	reactStrictMode: true,
+	compress: true,
+	images: {
+		domains: ["https://www.tejastn10.com"],
+	},
 };
 
 export default nextConfig;
