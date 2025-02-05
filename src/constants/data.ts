1c855ad0ad93ec3288d0db7d5cd3c6400ef43b79
Tejas Nikhar <tejastn10@gmail.com>
2025-02-05 07:19:29 +0530

    feat: add TECH enum for programming languages and tools

---
+++ src/constants/data.ts
@@ -0,0 +1,15 @@
+enum TECH {
+	JavaScript = "JavaScript",
+	TypeScript = "TypeScript",
+	Python = "Python",
+	Dart = "Dart",
+	Go = "Go",
+	Bash = "Bash",
+	Docker = "Docker",
+	Turborepo = "Turborepo",
+	NestJS = "NestJS",
+	GithubActions = "GithubActions",
+	ReduxSaga = "ReduxSaga",
+}
+
+export { TECH };
