ebab06a26d926d86aafa935c4bf356eb20b85a4a
Tejas Nikhar <tejastn10@gmail.com>
2025-02-26 00:02:46 +0530

    feat: enhance Navbar component with LineGrow for improved visual separation

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 import { ThemeToggle } from "@/components/theme/ThemeToggle";
 
+import { LineGrow } from "@/components/animated/LineGrow";
 import { Dock, DockIcon } from "@/components/animated/Dock";
 
-import { Separator } from "@/components/ui/Separator";
 import { buttonVariants } from "@/components/ui/Button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/Tooltip";
 
@@ -17,8 +17,17 @@ import { cx } from "@/utils/tailwind";
 const Navbar: FC = () => {
 	return (
 		<div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
-			<div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
-			<Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
+			<div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background" />
+
+			<Dock
+				className={cx(
+					"relative mx-auto flex min-h-full h-full items-center px-1 pointer-events-auto transform-gpu",
+					// Light mode styles
+					"bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+					// Dark mode styles
+					"dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.2)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
+				)}
+			>
 				{DATA.navbar.map((item, index) => (
 					<DockIcon key={index}>
 						<Tooltip>
@@ -36,7 +45,7 @@ const Navbar: FC = () => {
 						</Tooltip>
 					</DockIcon>
 				))}
-				<Separator orientation="vertical" className="h-full" />
+				<LineGrow className="h-full w-1/2 px-2" direction="vertical" />
 				{Object.entries(DATA.contact.social)
 					.filter(([, social]) => social.navbar)
 					.map(([name, social]) => (
@@ -58,7 +67,7 @@ const Navbar: FC = () => {
 							</Tooltip>
 						</DockIcon>
 					))}
-				<Separator orientation="vertical" className="h-full py-2" />
+				<LineGrow className="h-full w-1/2 px-2" direction="vertical" />
 				<DockIcon>
 					<Tooltip>
 						<TooltipTrigger asChild>
