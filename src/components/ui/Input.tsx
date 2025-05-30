91b20d8a983dd6d9dee41ad142d7c85d53e65c8f
Tejas Nikhar <tejastn10@gmail.com>
2025-05-30 08:46:00 +0530

    feat: enhance Input component with focus state and visual indicator

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,22 +1,42 @@
-import { forwardRef } from "react";
+import { useState, forwardRef } from "react";
 
 import { cx } from "@/utils/tailwind";
 
 const Input = forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 	({ className, type, ...props }, ref) => {
+		const [isFocused, setIsFocused] = useState(false);
+
+		const onFocus = () => {
+			setIsFocused(true);
+		};
+		const onBlur = () => {
+			setIsFocused(false);
+		};
+
 		return (
-			<input
-				type={type}
-				className={cx(
-					"flex h-10 w-full rounded-none border-0 border-b border-b-input bg-transparent px-3 py-1 text-base shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-b-2 focus-visible:border-b-black dark:focus-visible:border-b-white disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-					className
-				)}
-				ref={ref}
-				{...props}
-			/>
+			<div className="relative w-full">
+				<input
+					type={type}
+					className={cx(
+						"flex h-10 w-full rounded-none border-0 bg-transparent px-3 py-1 text-base shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+						className
+					)}
+					ref={ref}
+					onBlur={onBlur}
+					onFocus={onFocus}
+					{...props}
+				/>
+				<div
+					className={cx(
+						"absolute bottom-0 left-0 h-[2px] bg-gradient-to-r from-black to-transparent dark:from-white transition-all duration-300",
+						isFocused ? "w-full" : "w-0"
+					)}
+				/>
+			</div>
 		);
 	}
 );
+
 Input.displayName = "Input";
 
 export { Input };
